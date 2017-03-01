2b443f5d508b1603eb7b1934bfbe2d110076799e
Rui Guo <rguo@leancloud.rocks>
2017-03-01 21:42:18 +0800

    first commit

---
+++ src/clj_kafka_client/core.clj
@@ -0,0 +1,6 @@
+(ns clj-kafka-client.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_kafka_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-kafka-client.core-test
+  (:require [clojure.test :refer :all]
+            [clj-kafka-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
